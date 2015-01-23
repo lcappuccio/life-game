db2681f7409dc19253b7a950e083f668d0c18532
lcappuccio <9638971+username@users.noreply.github.com>
2015-01-23 23:09:08 +0100

    Creating tests for iterator

--- src/org/systemexception/lifegame/test/TestBoardIterator.java
+++ src/org/systemexception/lifegame/test/TestBoardIterator.java
@@ -5,17 +5,38 @@
 package org.systemexception.lifegame.test;
 
 import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
 import org.systemexception.lifegame.game.BoardIterator;
 import org.systemexception.lifegame.model.Board;
 
 public class TestBoardIterator {
 
-	BoardIterator sut;
+	private BoardIterator sut;
+	private Board board;
 
 	@Before
 	public void setUp() throws Exception {
-		Board board = new Board(3, 3);
-		sut = new BoardIterator(board);
+		board = new Board(3, 3);
+		sut = new BoardIterator();
+		System.out.println("Board at start");
 		board.printBoard();
+	}
+
+	@Test
+	@Ignore
+	public void iterateBoard() {
+		board = sut.iterateBoard(board);
+		System.out.println("Board at iteration");
+		board.printBoard();
+	}
+	
+	@Test
+	public void test10Iterations() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Board at iteration " + i);
+			board = sut.iterateBoard(board);
+			board.printBoard();
+		}
 	}
 }
