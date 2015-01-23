982dc4b5d3e16c2337e7d239ee1f159d54d50f00
lcappuccio <9638971+username@users.noreply.github.com>
2015-01-23 23:08:38 +0100

    Iterator method implemented

--- src/org/systemexception/lifegame/game/BoardIterator.java
+++ src/org/systemexception/lifegame/game/BoardIterator.java
@@ -10,11 +10,35 @@ public class BoardIterator {
 
 	private Board newBoard, oldBoard;
 
-	public BoardIterator(Board oldBoard) {
-		this.oldBoard = oldBoard;
-	}
-
 	public int FateOfCellAt(int i, int j) {
 		return oldBoard.countSurroungingLiveCells(i, j);
+	}
+	
+	public Board iterateBoard(Board oldBoard) {
+		this.oldBoard = oldBoard;
+		newBoard = new Board(oldBoard.getBoardRows(), oldBoard.getBoardCols());
+		for (int i = 0; i < oldBoard.getBoardCols(); i ++) {
+			for (int j = 0; j < oldBoard.getBoardRows(); j++) {
+				if (oldBoard.getCellAt(i, j).isAlive()) {
+					// Cell dies
+					if (oldBoard.countSurroungingLiveCells(i, j) < 2) {
+						newBoard.getCellAt(i, j).setCellDead();
+					}
+					// Cell lives but is already alive so no changes here
+//					if (board.countSurroungingLiveCells(i, j) >= 2 && board.countSurroungingLiveCells(i, j) <= 3) {
+//						
+//					}
+					if (oldBoard.countSurroungingLiveCells(i, j) > 3) {
+						newBoard.getCellAt(i, j).setCellDead();
+					}
+				} else {
+					// Cell becomes alive
+					if (oldBoard.countSurroungingLiveCells(i, j) == 3) {
+						newBoard.getCellAt(i, j).setCellAlive();
+					}
+				}
+			}
+		}
+		return newBoard;
 	}
 }
