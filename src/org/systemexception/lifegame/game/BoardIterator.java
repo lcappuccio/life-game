0ed77acbf83a3489153da2fa59bae2153fb97957
lcappuccio <9638971+username@users.noreply.github.com>
2015-01-24 23:55:14 +0100

    removed unused code and sysout

--- src/org/systemexception/lifegame/game/BoardIterator.java
+++ src/org/systemexception/lifegame/game/BoardIterator.java
@@ -4,9 +4,6 @@
  */
 package org.systemexception.lifegame.game;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
 import javax.swing.JComponent;
 
 import org.systemexception.lifegame.model.Board;
@@ -14,56 +11,25 @@ import org.systemexception.lifegame.model.Board;
 public class BoardIterator extends JComponent {
 
 	private static final long serialVersionUID = -136951560110034304L;
-	private Board newBoard, oldBoard;
+	private Board newBoard;
 
-	public int FateOfCellAt(int i, int j) {
-		return oldBoard.countSurroungingLiveCells(i, j);
-	}
-	
 	public Board iterateBoard(Board oldBoard) {
 		newBoard = oldBoard;
-		for (int i = 0; i < oldBoard.getBoardRows(); i ++) {
+		for (int i = 0; i < oldBoard.getBoardRows(); i++) {
 			for (int j = 0; j < oldBoard.getBoardCols(); j++) {
-				if (oldBoard.getCellAt(i, j).isAlive()) {
-					// Cell dies
-					if (oldBoard.countSurroungingLiveCells(i, j) < 2) {
-//						System.out.println("BING " + i + "," + j);
-						newBoard.getCellAt(i, j).setCellDead();
-					}
-					// Cell lives but is already alive so no changes here
-//					if (board.countSurroungingLiveCells(i, j) >= 2 && board.countSurroungingLiveCells(i, j) <= 3) {
-//						
-//					}
-					if (oldBoard.countSurroungingLiveCells(i, j) >= 3) {
-//						System.out.println("BONG " + i + "," + j);
-						newBoard.getCellAt(i, j).setCellDead();
-					}
-				} else {
+				// Cell dies
+				if ((oldBoard.getCellAt(i, j).isAlive())
+						&& (oldBoard.countSurroungingLiveCells(i, j) == 2 || oldBoard.countSurroungingLiveCells(i, j) == 3)) {
+					newBoard.getCellAt(i, j).setCellAlive();
+				} else  {
+					newBoard.getCellAt(i, j).setCellDead();
+				}
+				if (oldBoard.getCellAt(i, j).isDead() && oldBoard.countSurroungingLiveCells(i, j) == 3) {
 					// Cell becomes alive
-					if (oldBoard.countSurroungingLiveCells(i, j) == 3) {
-//						System.out.println("BANG " + i + "," + j);
-						newBoard.getCellAt(i, j).setCellAlive();
-					}
+					newBoard.getCellAt(i, j).setCellAlive();
 				}
 			}
 		}
 		return newBoard;
-	}
-	
-	private void resetBoard() {
-		for (int i = 0; i < newBoard.getBoardRows(); i++) {
-			for (int j = 0; j < newBoard.getBoardCols(); j++) {
-				newBoard.getCellAt(i, j).setCellDead();
-			}
-		}
-	}
-	
-	public void paintComponent(Graphics g) {
-		for (int i = 0; i < newBoard.getBoardRows(); i++) {
-			for (int j = 0; j < newBoard.getBoardCols(); j++) {
-				g.setColor(newBoard.getCellAt(i, j).isAlive() ? Color.DARK_GRAY : Color.WHITE);
-				g.fillRect(5 * i, 5 * j, 5, 10);
-			}
-		}
 	}
 }
