69d1d46d77416a217c3a65ce8e60e67c76a325fa
lcappuccio <9638971+username@users.noreply.github.com>
2015-01-26 20:18:55 +0100

    Set transparent textpane

--- src/org/systemexception/lifegame/gui/About.java
+++ src/org/systemexception/lifegame/gui/About.java
@@ -1,11 +1,13 @@
 package org.systemexception.lifegame.gui;
 
+import java.awt.Color;
+
 import javax.swing.JFrame;
 import javax.swing.JTextPane;
 
 @SuppressWarnings("serial")
 public class About extends JFrame {
-	
+
 	private static int windowWidth = 270, windowHeight = 90;
 
 	/**
@@ -17,13 +19,11 @@ public class About extends JFrame {
 		setBounds(100, 100, windowWidth, windowHeight);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
-		
 		JTextPane aboutText = new JTextPane();
 		aboutText.setEditable(false);
+		aboutText.setBackground(new Color(255, 255, 255, 0));
 		aboutText.setText("LifeGame by Leonardo Cappuccio\n\nReleased under GPL License 3.0 - 2014");
 		aboutText.setBounds(6, 6, 259, 52);
-		aboutText.setOpaque(false);
 		getContentPane().add(aboutText);
-
 	}
 }
