9dc55fcc29984044da50dbd13eaf3c08f01933e4
lcappuccio <9638971+username@users.noreply.github.com>
2015-01-26 21:14:49 +0100

    About window reordered, removed JTextPane and using separate JLabels

--- src/org/systemexception/lifegame/gui/About.java
+++ src/org/systemexception/lifegame/gui/About.java
@@ -1,14 +1,16 @@
 package org.systemexception.lifegame.gui;
 
-import java.awt.Color;
+import java.awt.Font;
 
 import javax.swing.JFrame;
-import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 @SuppressWarnings("serial")
 public class About extends JFrame {
 
 	private static int windowWidth = 270, windowHeight = 90;
+	private JLabel labelTitle, lavelCopyright, labelLicense;
 
 	/**
 	 * Create the frame.
@@ -19,11 +21,23 @@ public class About extends JFrame {
 		setBounds(100, 100, windowWidth, windowHeight);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		getContentPane().setLayout(null);
-		JTextPane aboutText = new JTextPane();
-		aboutText.setEditable(false);
-		aboutText.setBackground(new Color(255, 255, 255, 0));
-		aboutText.setText("LifeGame by Leonardo Cappuccio\n\nReleased under GPL License 3.0 - 2014");
-		aboutText.setBounds(6, 6, 259, 52);
-		getContentPane().add(aboutText);
+
+		labelTitle = new JLabel("LifeGame - Java Conway's Game of Life");
+		labelTitle.setFont(new Font("Lucida Grande", Font.BOLD, 12));
+		labelTitle.setHorizontalAlignment(SwingConstants.CENTER);
+		labelTitle.setBounds(6, 6, 258, 16);
+		getContentPane().add(labelTitle);
+
+		lavelCopyright = new JLabel("Copyright © 2014 - Leonardo Cappuccio");
+		lavelCopyright.setHorizontalAlignment(SwingConstants.CENTER);
+		lavelCopyright.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		lavelCopyright.setBounds(6, 25, 258, 16);
+		getContentPane().add(lavelCopyright);
+
+		labelLicense = new JLabel("Released under GNU GPL v3.0 License");
+		labelLicense.setHorizontalAlignment(SwingConstants.CENTER);
+		labelLicense.setFont(new Font("Lucida Grande", Font.PLAIN, 10));
+		labelLicense.setBounds(6, 46, 258, 16);
+		getContentPane().add(labelLicense);
 	}
 }
