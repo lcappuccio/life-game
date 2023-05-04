46e81d315e8c1807707a6f8292cea1ce6c015ee1
leo <leo@users.noreply.github.com>
2023-05-04 21:27:40 +0200

    use enums

--- src/main/java/org/systemexception/lifegame/enums/Automata.java
+++ src/main/java/org/systemexception/lifegame/enums/Automata.java
@@ -6,24 +6,13 @@ package org.systemexception.lifegame.enums;
 
 public enum Automata {
 
-	ASSIMILATION("Assimilation"),
-	CONWAY("Conway's Life"),
-	CORAL("Coral"),
-	DRYLIFE("DryLife"),
-	HIGHLIFE("HighLife"),
-	LIVEFREEORDIE("Live Free or Die"),
-	MAZE("Maze"),
-	MOVE("Move"),
-	SERVIETTES("Serviettes");
-
-	private final String automataName;
-
-	Automata(final String automataName) {
-		this.automataName = automataName;
-	}
-
-	@Override
-	public String toString() {
-		return automataName;
-	}
+	ASSIMILATION,
+	CONWAY,
+	CORAL,
+	DRYLIFE,
+	HIGHLIFE,
+	LIVEFREEORDIE,
+	MAZE,
+	MOVE,
+	SERVIETTES
 }
